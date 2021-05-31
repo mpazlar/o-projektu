54da1cca26187c3139fcd99fe07e5ead6e19a572
Magdaléna Pazlarová <m.pazlarova@gmail.com>
2021-05-31 16:21:09 +0200

    rozdělanej footer

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -2,5 +2,32 @@ import React from 'react';
 import './style.css';
 
 export const Footer = () => {
-  return <p className="footer">Vytvořeno pro Czechitas</p>;
+  return (
+    <footer className="footer">
+      <div>
+        <a href="https://www.czechitas.cz/cs/" target="_blank">
+          <img
+            className="footer__img"
+            src="https://digikoalice.cz/wp-content/uploads/2018/08/Czechitas_logo_new.png"
+            alt="Czechitas logo"
+          />
+        </a>
+        <h5>Vytvořeno pro Czechitas©</h5>
+      </div>
+      <p className="footer__email">
+        <h5>Napište nám!</h5>
+        <h5>
+          Majda:
+          <a href="mailto:m.pazlarova@gmail.com">m.pazlarova@gmail.com</a>
+        </h5>
+
+        <h5>
+          Verča:
+          <a href="mailto:prokupkova.verca@gmail.com">
+            prokupkova.verca@gmail.com
+          </a>
+        </h5>
+      </p>
+    </footer>
+  );
 };
--- src/Majda/index.jsx
+++ src/Majda/index.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 import './style.css';
 export const Majda = () => {
-  return <h1 className="nadpis">Majda</h1>;
+  return (
+    <div>
+      <h1>Majda</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias possimus
+        inventore sequi ipsam. Aut, voluptatum. Odio, repellat. Quis veniam
+        optio consectetur, voluptatibus deserunt, facere corrupti, ducimus modi
+        quibusdam maxime nihil.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae
+        enim vel asperiores harum eligendi tempore, eum fugiat sunt nulla quae
+        placeat, repudiandae ad aliquid quod earum officia eius quidem?
+        Exercitationem!
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis nisi
+        explicabo magni enim officia! Pariatur eaque fuga reprehenderit odit
+        possimus eum magnam voluptatum rem consequuntur aliquid, id veniam odio
+        placeat?
+      </p>
+    </div>
+  );
 };
